1708355d455e86828e8ff9eab0ed4614610d0483
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2022-09-26 16:10:51 -0300

    opção deletar no modal categoria

--- frontend/spyfall-br-adm/src/components/ModalCategory/index.tsx
+++ frontend/spyfall-br-adm/src/components/ModalCategory/index.tsx
@@ -15,40 +15,77 @@ export default function ModalCategory({modalCategoryProperties, setModalCategory
 
     const {show, type, currentValue} = modalCategoryProperties
     let category = currentValue
-    const handleClose = () => setModalCategoryProperties({...modalCategoryProperties, show: false});
+    const handleClose = () => setModalCategoryProperties({...modalCategoryProperties, show: false})
+    const titleMap = {
+        create: 'Criando nova categoria',
+        update: 'Editando categoria',
+        delete: `Deletar a categoria "${category.name}" ?`
+    }
+    const btnLabelMap = {
+        create: 'Criar categoria',
+        update: 'Salvar categoria',
+        delete: 'Deletar categoria'
+    }
+    const functionMap = {
+        create: createCategory,
+        update: updateCategory,
+        delete: deleteCategory
+    }
 
     return (
         <Modal show={show} onHide={handleClose}>
             <Modal.Header closeButton>
                 <Modal.Title>
-                    {(type === 'create') ?
-                     ("Criando nova categoria") :
-                     ("Editando categoria")}
+                    {titleMap[type]}
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                    <Form.Label>Nome</Form.Label>
-                    <Form.Control 
-                        type="text"
-                        onChange={(event) => {
-                            category.name = event.target.value
-                            setModalCategoryProperties({...modalCategoryProperties, currentValue: category})
-                        }}
-                        value={category.name}
-                    />
-                </Form.Group>
+                {(type === 'delete') && 
+                    <p>Deseja mesmo excluir "{category.name}" ?</p>
+                }
+
+                {(type === 'create' || type === 'update') &&
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                        <Form.Label>Nome</Form.Label>
+                        <Form.Control 
+                            type="text"
+                            onChange={(event) => {
+                                category.name = event.target.value
+                                setModalCategoryProperties({...modalCategoryProperties, currentValue: category})
+                            }}
+                            value={category.name}
+                        />
+                    </Form.Group>
+                }
             </Modal.Body>
             <Modal.Footer>
                 <Button variant="secondary" onClick={handleClose}>
                     Fechar
                 </Button>
-                <Button variant="primary" onClick={handleClose}>
-                    {(type === 'create') ?
-                     ('Criar categoria') :
-                     ('Salvar atualizações')}
+                <Button 
+                    variant={(type === 'delete') ? ('danger') : ('primary')}
+                    onClick={() => {
+                        functionMap[type](category, setCategories)
+                        handleClose()
+                    }}
+                >
+                    {btnLabelMap[type]}
                 </Button>
             </Modal.Footer>
         </Modal>
     )
+}
+
+function createCategory(category: Category, setCategories: React.Dispatch<React.SetStateAction<Category[]>>) {
+    // Para atualizar o frontend
+    
+}
+
+function updateCategory(category: Category, setCategories: React.Dispatch<React.SetStateAction<Category[]>>) {
+    // Para atualizar o frontend
+}
+
+function deleteCategory(category: Category, setCategories: React.Dispatch<React.SetStateAction<Category[]>>) {
+
+    // Para atualizar o frontend
 }
