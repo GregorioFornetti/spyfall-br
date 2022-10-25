9ae4a42cb9c2bc23107fa1aca017c8cab995a773
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2022-10-25 11:56:59 -0300

    conexao com bd AWS

--- backend/src/db-api/models/connection.js
+++ backend/src/db-api/models/connection.js
@@ -1,17 +1,36 @@
 import { Sequelize } from 'sequelize'
+import mysql from 'mysql2/promise'
 
-const sequelize = new Sequelize('spyfall-br', 'postgres', 'secreto123', {
-    dialect: 'postgres',
-    host: 'localhost',
-    port: '5432',
-    password: 'secreto123'
-})
+async function createConnection() {
+    return mysql.createConnection({
+        host: 'spy-fall-br.cq0ijpvqeyuk.sa-east-1.rds.amazonaws.com',
+        port: '3306',
+        user: 'admin',
+        password: 'greglol123',
+    }).then(async (connection) => {
+        return connection.query('CREATE DATABASE IF NOT EXISTS spyfallbr;')
+        .then(async () => {
+            const sequelize = new Sequelize('spyfallbr', 'admin', 'greglol123', {
+                dialect: 'mysql',
+                host: 'spy-fall-br.cq0ijpvqeyuk.sa-east-1.rds.amazonaws.com',
+                port: '3306',
+                logging: console.log,
+                maxConcurrentQueries: 100,
+                ssl: 'Amazon RDS',
+                pool: { maxConnections: 5, maxIdleTime: 30},
+                language: 'en'
+            })
+            
+            try {
+                await sequelize.authenticate()
+                return sequelize
+            } catch(error) {
+                console.error('deu ruim', error)
+                return null
+            }
+        })
+    })
 
-try {
-    await sequelize.authenticate()
-    console.log('Deu bom')
-} catch(error) {
-    console.error('deu ruim', error)
 }
-
-export default sequelize;
+    
+export default await createConnection();
